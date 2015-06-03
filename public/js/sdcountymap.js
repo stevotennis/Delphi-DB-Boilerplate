1298475e41a1821b6ccaca170dbca232d16ab81e
Jonathan Mariano <jamarian@ucsd.edu>
2015-06-03 12:19:02 -0700

    SD county map displaying

---
+++ public/js/sdcountymap.js
@@ -0,0 +1,49 @@
+var map = new Datamap({
+    element: document.getElementById('sdmap'),
+    done: function(datamap) {
+            datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
+                var id = geography.id;
+                console.log("id: " + id);
+                testcolor('#000000', id);
+                updateLabel('neighborhood', geography.properties.name);
+            });
+        },
+    geographyConfig: {
+      dataUrl: 'maps/sdcounty.topo.json',
+      highlightFillColor: '#CCCCCC',
+      highlightBorderColor: '#'
+    },
+    scope: 'sdcounty',
+    fills: {
+      defaultFill: '#6699FF',
+      someKey: '#6699FF'
+    },
+    data: {
+      '1': {fillKey: 'someKey'},
+      '2': {fillKey: 'someKey'}
+    },
+    setProjection: function(element) {
+
+      var projection = d3.geo.mercator()
+
+        .center([-116.746, 33.004])
+        .scale(15000)
+        .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+
+       var path = d3.geo.path().projection(projection);
+       return {path: path, projection: projection};
+    }
+  });
+
+function testcolor(color, id){
+  console.log("id: " + id + " color: " + color);
+  map.updateChoropleth({
+    id: color
+  });
+}
+
+function updateLabel(label, field) {
+  var element = document.getElementById(label);
+  console.log("chosen: " + field);
+  element.innerHTML = field;
+}
